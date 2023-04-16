37c1d53b728312fa1a1bf11a6fa6a1e6362ee4bf
JRBoland <james.boland94@hotmail.com>
2023-04-16 21:14:27 +1000

    updated stying and hyperlinks on episodes h3

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,5 +1,5 @@
 const panels = document.querySelectorAll('.panel')
-
+const heading = document.querySelectorAll('h3')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
@@ -13,3 +13,17 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+function darkMode() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+
+    let btn = document.getElementById("dark-mode-btn");
+    if (btn.textContent === "GO DARK") {
+        btn.textContent = "GO LIGHT";
+    } else {
+        btn.textContent = "GO DARK"
+    }
+    }
+
+
